43cb51cba89f0f697eb5f96073ee0841bdf94ea5
Paulo Vieira <88034847+spacetk00@users.noreply.github.com>
2022-09-23 19:34:34 -0300

    Adicionada responsividade no modal de cadastro

--- web/src/components/CreateAdModal.tsx
+++ web/src/components/CreateAdModal.tsx
@@ -57,13 +57,13 @@ export function CreateAdModal() {
     <Dialog.Portal>
       <Dialog.Overlay className="bg-black/80 inset-0 fixed" />
 
-      <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25">
-        <Dialog.Title className="text-3xl font-black">
+      <Dialog.Content className="fixed bg-[#2A2634] py-8 px-4 md:px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[320px] sm:w-[480px] shadow-lg shadow-black/25">
+        <Dialog.Title className="text-xl text-center sm:text-left sm:text-3xl font-black">
           Publique um anúncio
         </Dialog.Title>
 
         <form onSubmit={handleCreateAd} className="mt-8 flex flex-col gap-4">
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-auto flex-col gap-2">
             <label htmlFor="game" className="font-semibold">
               Qual o game?
             </label>
@@ -96,7 +96,7 @@ export function CreateAdModal() {
             />
           </div>
 
-          <div className="grid grid-cols-2 gap-6">
+          <div className="justify-center sm:grid-cols-2 md:grid gap-6">
             <div className="flex flex-col gap-2">
               <label htmlFor="yearsPlaying">Joga há quantos anos?</label>
               <Input
@@ -112,13 +112,13 @@ export function CreateAdModal() {
             </div>
           </div>
 
-          <div className="flex gap-6">
-            <div className="flex flex-col gap-2">
+          <div className="flex-col sm:flex-row justify-center items-center">
+            <div className="flex flex-col gap-2 pb-5">
               <label htmlFor="weekDays">Quando costuma jogar?</label>
 
               <ToggleGroup.Root
                 type="multiple"
-                className="grid grid-cols-4 gap-2"
+                className="flex flex-wrap gap-2"
                 value={weekDays}
                 onValueChange={setWeekDays}
               >
@@ -228,7 +228,7 @@ export function CreateAdModal() {
           <footer className="mt-4 flex justify-end gap-4">
             <Dialog.Close
               type="button"
-              className="bg-zinc-500 px-5 h-12 rounded-md font-semibold"
+              className="bg-zinc-500 px-2 sm:px-5 h-12 rounded-md font-semibold"
             >
               Cancelar
             </Dialog.Close>
